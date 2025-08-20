883ab08b36b10a91205a2b5b1ce17d732e74f78a
Angel <alconangel473@gmail.com>
2025-08-20 12:57:53 -0300

    agregacion de bbdd

---
+++ BBDDcontacto.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE mi_contacto;
+USE mi_contacto
+
+CREATE TABLE contactos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    mensaje TEXT NOT NULL,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+)
